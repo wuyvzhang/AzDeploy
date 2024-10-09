c66c640900879438bb64786347bcaa3cebb074d9
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2024-10-09 13:38:06 +0000

    c1

---
+++ prod/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "githubazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
