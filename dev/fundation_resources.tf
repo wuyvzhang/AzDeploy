a407de6b80d0235829aa5424bf8585056cf39c22
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2024-10-09 13:58:53 +0000

    c1

--- dev/fundation_resources.tf
+++ dev/fundation_resources.tf
@@ -12,17 +12,58 @@ resource "azurerm_resource_group" "rg" {
 }
 
 #Create a Key Vault for the Resource Group
-resource "azurerm_key_vault" "kv" {
-  name                        = "${lower(var.key_vault_name)}${random_integer.kv_num.result}"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enable_rbac_authorization   = var.use_rbac_mode
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name                    = "standard"
-  tags                        = var.tags
+resource "azurerm_key_vault" "example" {
+  name                       = "${lower(var.key_vault_name)}${random_integer.kv_num.result}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Delete",
+      "Get",
+      "Purge",
+      "Recover",
+      "Update",
+      "GetRotationPolicy",
+      "SetRotationPolicy"
+    ]
+
+    secret_permissions = [
+      "Set",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_key" "generated" {
+  name         = "${lower(var.key_vault_name)}${random_integer.kv_num.result}"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
+  }
 }
 
 resource "random_integer" "kv_num" {
