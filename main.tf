9b529a33322c79ff8656dd1d5b2d51b0599fb287
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2024-10-10 12:53:02 +0000

    m4

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-
-  backend "azurerm" {
-    resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "githubazuretf"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-  }
   required_version = ">=1.0"
   required_providers {
     azurerm = {
@@ -20,4 +13,13 @@ terraform {
 }
 provider "azurerm" {
   features {}
+}
+
+terraform{
+  backend "azurerm" {
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "githubazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
 }
