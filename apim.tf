ed3420e4874406f57b994eb0de4be5e2b8302813
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2024-10-10 09:18:18 +0000

    apim

--- apim.tf
+++ apim.tf
@@ -1,17 +1,13 @@
 resource "azurerm_api_management" "apim01" {
-  name                = "example-apim"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  publisher_name      = "My Company"
-  publisher_email     = "company@terraform.io"
-
-  sku_name = "Developer_1"
-
+  name                          = "example-apim"
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  publisher_name                = "My Company"
+  publisher_email               = "company@terraform.io"
+  sku_name                      = "Developer_1"
   public_network_access_enabled = false
-
-  virtual_network_type = "Internal"
-
+  virtual_network_type          = "Internal"
   virtual_network_configuration {
-      subnet_id = "/subscriptions/177add63-2747-4d6d-a5e4-004dc63b04c6/resourceGroups/RG-W2K16-Demo-JPE/providers/Microsoft.Network/virtualNetworks/SR-W2k16-01-vnet/subnets/apim"
+    subnet_id = "/subscriptions/177add63-2747-4d6d-a5e4-004dc63b04c6/resourceGroups/RG-W2K16-Demo-JPE/providers/Microsoft.Network/virtualNetworks/SR-W2k16-01-vnet/subnets/apim"
   }
 }
