20edd244799a0931c26be12dbb311682da086316
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2024-10-10 10:00:53 +0000

    update apim

--- apim.tf
+++ apim.tf
@@ -1,5 +1,5 @@
 resource "azurerm_api_management" "apim01" {
-  name                          = "example-apim"
+  name                          = var.apim01_name
   location                      = var.location
   resource_group_name           = var.resource_group_name
   publisher_name                = "My Company"
