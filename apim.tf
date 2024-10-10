38f4a2e069fc80e478e0b223b7487daf6cb105f3
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2024-10-10 15:30:33 +0000

    m5

--- apim.tf
+++ apim.tf
@@ -4,7 +4,7 @@ resource "azurerm_api_management" "apim" {
   resource_group_name           = var.resource_group_name
   publisher_name                = "My Company"
   publisher_email               = "company@terraform.io"
-  sku_name                      = "Standard"
+  sku_name                      = 'StandardV2'
   virtual_network_type          = "External"
   virtual_network_configuration {
     subnet_id = "/subscriptions/177add63-2747-4d6d-a5e4-004dc63b04c6/resourceGroups/RG-W2K16-Demo-JPE/providers/Microsoft.Network/virtualNetworks/SR-W2k16-01-vnet/subnets/apim"
